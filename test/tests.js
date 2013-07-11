f17f2ea145a1316fb80a8c3d6a1c54470d42da66
Katie Gengler <katie@kmg.io>
2013-07-11 03:25:22 -0400

    More tests

--- test/tests.js
+++ test/tests.js
@@ -2,6 +2,18 @@ Pg.rootElement = '#ember-testing';
 Pg.setupForTesting();
 Pg.injectTestHelpers();
 
+function importGallery(options) {
+  var defaults = {title: 'Planting Fields',
+                  url: 'https://dl.dropboxusercontent.com/u/38141173/galleries/planting_fields',
+                  numPhotos: '3'};
+  var opts = $.extend({}, defaults, options);
+  return visit('/galleries/new')
+    .fillIn('.js-title-input', opts.title)
+    .fillIn('.js-url-input', opts.url)
+    .fillIn('.js-num-photos-input', opts.numPhotos)
+    .click("button:contains('Import Photos')");
+}
+
 module("Integration tests", {
   setup: function() {
     window.localStorage.clear();
@@ -19,10 +31,7 @@ test("Adding a gallery should have page subheading", function() {
 });
 
 test("Importing a gallery should show spinner on Import button", function(){
-  visit('/galleries/new')
-    .fillIn('.js-title-input', 'Planting Fields')
-    .fillIn('.js-url-input', 'https://dl.dropboxusercontent.com/u/38141173/galleries/planting_fields')
-    .fillIn('.js-num-photos-input', '2')
+  importGallery({numPhotos: 2})
     .then(function(){
       click("button:contains('Import Photos')");
       ok(find('.icon-spin5.animate-spin').length, "The button should spin when clicked");
@@ -34,12 +43,42 @@ test("Importing a gallery should show spinner on Import button", function(){
 });
 
 test("Importing a gallery should display photos", function(){
-   visit('/galleries/new')
-     .fillIn('.js-title-input', 'Planting Fields')
-     .fillIn('.js-url-input', 'https://dl.dropboxusercontent.com/u/38141173/galleries/planting_fields')
-     .fillIn('.js-num-photos-input', '3')
-     .click("button:contains('Import Photos')")
+   importGallery()
      .then(function(){
         equal(find('img').length, 3, "3 Photos should display")
      });
 });
+
+test("Can delete photo from imported photos", function(){
+   importGallery({numPhotos: 3})
+    .click("a.icon-cancel:first")
+    .then(function(){
+      equal(find('img').length, 2, "2 Photos should display, 1 having been removed");
+      return wait();
+    })
+    .click("button:contains('Create Gallery')")
+    .click("a:contains('Planting Fields')")
+    .then(function(){
+      equal(find('a img.th').length, 2, "Only 2 photos should have saved");
+    });
+});
+
+test("Creating a gallery should create it and display the galleries", function(){
+  var galleryTitle = "Gallery title in test";
+  importGallery({title: galleryTitle})
+    .click("button:contains('Create Gallery')")
+    .then(function(){
+      ok(find("a:contains('"+galleryTitle+"')").length, "Should have link to gallery with thumbnail");
+    });
+});
+
+test("Creating a gallery should create it and be correct", function(){
+  var galleryTitle = "Gallery title in test for other values";
+  importGallery({title: galleryTitle, numPhotos: 5})
+    .click("button:contains('Create Gallery')")
+    .click("a:contains('"+galleryTitle+"')")
+    .then(function(){
+      equal(find('a img.th').length, 5, "The number of imported photos should display");
+    });
+});
+
